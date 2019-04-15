b07069cd4088772352d3649dcc12056ef6f87ed2
soaresCecilia <cecilia.oliveira.soares@gmail.com>
2019-04-15 20:35:17 +0100

    Adicionei a função main

--- ma.c
+++ ma.c
@@ -161,3 +161,20 @@ int insereArtigo(char *preco, char *nome){
   return bytesEscritos;
 
 }
+
+
+int main(int argc, char *argv[]) {
+
+  if(argc < 4) {
+    perror("Número de argumentos insuficientes. Tem de passar 4 argumentos.");
+    _exit(0);
+  }
+
+  if(argc == 4 && (strcmp(argv[1], "i") == 0)){
+      criaFicheiros();
+      insereArtigo(argv[3], argv[2]);
+
+  }
+
+  return 0;
+}
