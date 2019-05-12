5acf6ee245db37f0fed16f9d7100c6d593ea0333
soaresCecilia <cecilia.oliveira.soares@gmail.com>
2019-05-12 18:25:37 +0100

    Gerador de artigos.

--- Makefile
+++ Makefile
@@ -3,20 +3,23 @@ CFLAGS = -W -Wall -g
 
 all: ma sv cv ag agf
 
-ma: debug aux
+ma: debug aux ma.c
 	$(CC) $(CFLAGS) ma.c aux.o debug.o -o ma
 
-sv: debug aux
+sv: debug aux sv.c
 	$(CC) $(CFLAGS) sv.c aux.o debug.o -o sv
 
-cv: debug aux
+cv: debug aux cv.c
 	$(CC) $(CFLAGS) cv.c aux.o debug.o -o cv
 
-ag: debug aux
+ag: debug aux ag.c
 	$(CC) $(CFLAGS) ag.c aux.o debug.o -o ag
 
-agf: debug aux
+agf: debug aux agf.c
 	$(CC) $(CFLAGS) agf.c aux.o debug.o -o agf
+
+gerar-artigos: gerar-artigos.c
+	$(CC) $(CFLAGS) gerar-artigos.c -o gerar-artigos
 
 debug:
 	$(CC) $(CFLAGS) -c debug.c
@@ -25,7 +28,7 @@ aux: debug aux
 	$(CC) $(CFLAGS) -c aux.c
 
 clean:
-	rm -f *.o ma sv cv ag agf
+	rm -f *.o ma sv cv ag agf gerar-artigos
 
 clean-all-data:
 	rm -f vendas artigos strings stocks log_* > /dev/null 2>&1 
---
+++ gerar-artigos.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+
+const char *nome_base[] = {
+  "chocolate",
+  "caderno",
+  "chavena",
+  "mochila",
+  "telemóvel",
+  "guitarra",
+  "bolo",
+  "estrado",
+  "bicicleta",
+  "caneta",
+  "lápis",
+  "automóvel",
+  "prateleira",
+  "impressora",
+  "caneca"
+};
+
+const float preco_base[] = {
+  10.00,
+  11.00,
+  12.00,
+  13.00,
+  14.00,
+  15.00,
+  17.00,
+  18.00,
+  19.00,
+  20.00,
+  21.00,
+  22.00,
+  23.00,
+  24.00,
+  25.00
+};
+
+int main(int argv, char **argc) {
+  int size_nome = sizeof(nome_base)/sizeof(nome_base[0]);
+  int size_preco = sizeof(preco_base)/sizeof(preco_base[0]);
+  assert(size_nome == size_preco);
+
+  int num = 1;
+  if (argv > 1) {
+    num = atoi(argc[1]);
+  }
+
+  num = num * size_nome;
+  for (int it = 0; it < num ; it++) {
+    int idx = it % size_nome;
+    printf("i %s-%d %7.2f\n", nome_base[idx], it, preco_base[idx]);
+  }
+  return 0;
+}
